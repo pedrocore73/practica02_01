f0704efb268c9059c9c28132abeb75627206a4f3
pedrocore73 <pjimenez@corenetworks.es>
2019-10-25 15:07:44 +0200

    clientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { ReportesComponent } from './reportes/reportes.component';
+import { ClientesComponent } from './clientes/clientes.component';
 
 const routes: Routes = [
   {path: '', component: InicioComponent},
-  {path: 'reportes', component: ReportesComponent}
+  {path: 'reportes', component: ReportesComponent},
+  {path: 'clientes', component: ClientesComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BarraSuperiorComponent } from './barra-superior/barra-superior.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { ReportesComponent } from './reportes/reportes.component';
+import { ClientesComponent } from './clientes/clientes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarraSuperiorComponent,
     InicioComponent,
-    ReportesComponent
+    ReportesComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule,
